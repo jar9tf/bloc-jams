68f952ec8a8c24bcefd4bd40c2424e950bdd5d4e
Justin <jar9tf@yahoo.com>
2017-07-26 12:29:34 -0500

    Checkpoint 13: Pause button disappears and goes back to play button when the mouse is moved to the next line.

--- scripts/album.js
+++ scripts/album.js
@@ -61,6 +61,58 @@ var setCurrentAlbum = function album(album) {
  	}
  };
 
+ var findParentByClassName = function(element, targetClass){
+ 	if (element)
+ 	{
+ 		var currentParent = element.parentElement;
+
+ 		while(currentParent.className !== targetClass && currentParent.className !== null) {
+ 			currentParent = currentParent.parentElement;
+ 		}
+
+ 		return currentParent;
+ 	}
+ };
+
+var getSongItem = function(element) {
+    switch (element.className) {
+        case 'album-song-button':
+        case 'ion-play':
+        case 'ion-pause':
+            return findParentByClassName(element, 'song-item-number');
+        case 'album-view-song-item':
+            return element.querySelector('.song-item-number');
+        case 'song-item-title':
+        case 'song-item-duration':
+            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+        case 'song-item-number':
+            return element;
+        default:
+            return;
+    }  
+};
+
+ var clickHandler = function(targetElement){
+ 	var songItem = getSongItem(targetElement);
+
+ 	if (currentlyPlayingSong === null){
+ 		songItem.innerHTML = pauseButtonTemplate;
+ 		currentPlayingSong = songItem.getAttribute("data-song-number");
+ 	
+	
+ 	}else if (currentlyPlayingSong === songItem.getAttribute("data-song-number")) {
+ 		songItem.innerHTML = playButtonTemplate;
+ 		currentPlayingSong = null;
+ 	
+
+ 	}else if (currentlyPlayingSong !== songItem.getAttribute("data-song-number")) {
+ 		var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
+ 		currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute("data-song-number");
+ 		songItem.innerHTML = pauseButtonTemplate;
+ 		currentlyPlayingSong = songItem.getAttribute("data-song-number");
+ 	}
+ };
+
  	var albumTitle = document.getElementsByClassName("album-view-title")[0];					// Why did these declarations have to come out of the setCurrentAlbum function?
  	var albumArtist = document.getElementsByClassName("album-view-artist")[0];
  	var albumReleaseInfo = document.getElementsByClassName("album-view-release-info")[0];
@@ -87,6 +139,9 @@ var setCurrentAlbum = function album(album) {
 var songRows = document.getElementsByClassName("album-view-song-item");
 
  var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+ var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></a>';
+
+ var currentlyPlayingSong = null;
 
  window.onload = function() 
  {
@@ -95,14 +150,29 @@ var songRows = document.getElementsByClassName("album-view-song-item");
  	songListContainer.addEventListener('mouseover', function(event) {
 
  		if (event.target.parentElement.className === 'album-view-song-item') {
-             event.target.parentElement.querySelector(".song-item-number").innerHTML = playButtonTemplate;
+             var songItem = getSongItem(event.target);
+
+            if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
+               songItem.innerHTML = playButtonTemplate;
+            }
          }
 
  	});
 
  	for (i = 0; i < songRows.length; i++){
  		songRows[i].addEventListener("mouseleave", function(event) {
- 			this.children[0].innerHTML = this.children[0].getAttribute("data-song-number");
+
+ 		var songItem = getSongItem(event.target);
+        var songItemNumber = songItem.getAttribute('data-song-number');
+ 		if (songItemNumber !== currentlyPlayingSong) {
+                 songItem.innerHTML = songItemNumber;
+             }
+
+ 		});
+
+
+ 		songRows[i].addEventListener("click", function(event){
+ 			clickHandler(event.target);
  		});
  	}
 
