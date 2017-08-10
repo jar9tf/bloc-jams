427ce76cf90778108b52ac7ad2065be90e3f4d03
Justin <jar9tf@yahoo.com>
2017-08-10 14:51:00 -0500

    checkpoint-18-jQuery-Play/Pause: completed

--- scripts/album.js
+++ scripts/album.js
@@ -68,81 +68,6 @@ var setCurrentAlbum = function album(album) {
  	}
  };
 
- var findParentByClassName = function(element, targetClass){
- 	if (element)
- 	{
-        if (element.parentElement && element.parentElement.className) {
-
- 		if(element.parentElement === null)
- 		{
- 			console.log("No parent found");
- 		}
-
- 		else if(element.parentElement.className !== targetClass)
- 		{
- 			console.log("No parent found with that class name.");
- 		}
-
- 		else if(element.parentElement !== null && element.parentElement.className === targetClass)
- 		{
- 			var currentParent = element.parentElement;
- 		}
-
- 		debugger;
-
-
- 		while(currentParent.className !== targetClass && currentParent.className !== null) {
- 			currentParent = currentParent.parentElement;
- 		}
-
- 		return currentParent;
- 	}
- }
-
- };
-
-
-
-var getSongItem = function(element) {
-    switch (element.className) {
-        case 'album-song-button':
-        case 'ion-play':
-        case 'ion-pause':
-            return findParentByClassName(element, 'song-item-number');
-        case 'album-view-song-item':
-            return element.querySelector('.song-item-number');
-        case 'song-item-title':
-        case 'song-item-duration':
-            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
-        case 'song-item-number':
-            return element;
-        default:
-            return;
-    }  
-};
-
- var clickHandler = function(targetElement){
- 	
- 	var songItem = getSongItem(targetElement);
-
- 	if (currentlyPlayingSong === null){
- 		songItem.innerHTML = pauseButtonTemplate;
- 		currentlyPlayingSong = songItem.getAttribute("data-song-number");
- 	
-	
- 	}else if (currentlyPlayingSong === songItem.getAttribute("data-song-number")) {
- 		songItem.innerHTML = playButtonTemplate;
- 		currentlyPlayingSong = null;
- 	
-
- 	}else if (currentlyPlayingSong !== songItem.getAttribute("data-song-number")) {
- 		var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
- 		currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute("data-song-number");
- 		songItem.innerHTML = pauseButtonTemplate;
- 		currentlyPlayingSong = songItem.getAttribute("data-song-number");
- 	}
- };
-
  	var albumTitle = document.getElementsByClassName("album-view-title")[0];					// Why did these declarations have to come out of the setCurrentAlbum function?
  	var albumArtist = document.getElementsByClassName("album-view-artist")[0];
  	var albumReleaseInfo = document.getElementsByClassName("album-view-release-info")[0];
@@ -164,10 +89,23 @@ var getSongItem = function(element) {
  	var $row = $(template);
 
  	var clickHandler = function() {
- 		//clickHandler Logic
- 		
+	var songNumber = $(this).attr('data-song-number');
 
- 	};
+	if (currentlyPlayingSong !== null) {
+		// Revert to song number for currently playing song because user started playing new song.
+		var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSong + '"]');
+		currentlyPlayingCell.html(currentlyPlayingSong);
+	}
+	if (currentlyPlayingSong !== songNumber) {
+		// Switch from Play -> Pause button to indicate new song is playing.
+		$(this).html(pauseButtonTemplate);
+		currentlyPlayingSong = songNumber;
+	} else if (currentlyPlayingSong === songNumber) {
+		// Switch from Pause -> Play button to pause currently playing song.
+		$(this).html(playButtonTemplate);
+		currentlyPlayingSong = null;
+	}
+};
 
    var onHover = function(event) {
         var songNumberCell = $(this).find('.song-item-number');
@@ -194,9 +132,6 @@ var getSongItem = function(element) {
  	return $row;
  };
 
- var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
-
-var songRows = document.getElementsByClassName("album-view-song-item");
 
  var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
  var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></a>';
@@ -206,14 +141,7 @@ var songRows = document.getElementsByClassName("album-view-song-item");
 $(document).ready(function()
  {
  	setCurrentAlbum(albumPicasso);
-
- 	for (i = 0; i < songRows.length; i++){
-
- 		songRows[i].addEventListener("click", function(event){
- 			clickHandler(event.target);
- 		});
- 	}
-
+ 	
  	var albums = [albumPicasso, albumMarconi, albumJustin];
  	var count = 1;
 
